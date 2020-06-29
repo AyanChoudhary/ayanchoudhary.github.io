37a0f3ddae8a55f68b60629c37b3bccf57c47a3a
Ayan Choudhary <darkrider251099@gmail.com>
2020-06-29 20:34:03 +0530

    Updates

---
+++ precache-manifest.a81e79526342dd6e7083feef4d2483ab.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f65e0ab40623c0817e67e184cebba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7811ba4be4dfc7494969",
+    "url": "/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "1662f996ba58701d6cea",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "1d49c1a896ff4418addf",
+    "url": "/static/css/main.c7e68b92.chunk.css"
+  },
+  {
+    "revision": "810490d5e4cb50d6979c",
+    "url": "/static/js/2.0824d1dc.chunk.js"
+  },
+  {
+    "revision": "7811ba4be4dfc7494969",
+    "url": "/static/js/3.08c1b4fc.chunk.js"
+  },
+  {
+    "revision": "1662f996ba58701d6cea",
+    "url": "/static/js/4.8e35f6f6.chunk.js"
+  },
+  {
+    "revision": "1d49c1a896ff4418addf",
+    "url": "/static/js/main.0157e4e6.chunk.js"
+  },
+  {
+    "revision": "54d67138ed5e993ddadd",
+    "url": "/static/js/runtime-main.e45f10e9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "43fe96a6b33d5475bc92d78ef8e81326",
+    "url": "/static/media/cc.43fe96a6.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "47374b7a613f5125f997a5473c94dab0",
+    "url": "/static/media/gsoc.47374b7a.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c70aa82ef200e9c46b874c61789f28bb",
+    "url": "/static/media/microsoft.c70aa82e.webp"
+  },
+  {
+    "revision": "20b645cb3b217bfd03e86cddb6632a10",
+    "url": "/static/media/publiclab.20b645cb.webp"
+  },
+  {
+    "revision": "9d030f48299f8ef53a35ff005e5354b6",
+    "url": "/static/media/sds.9d030f48.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "d237c561ba3c34e3d34d7a9802e98f53",
+    "url": "/static/media/spark.d237c561.webp"
+  }
+]);
