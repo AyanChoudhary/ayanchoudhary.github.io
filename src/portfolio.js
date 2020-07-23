ae7c6c077f74c593b401832675794ce1ec1d3289
Ayan Choudhary <darkrider251099@gmail.com>
2020-07-23 11:21:21 +0530

    blog: enter bog details

--- src/portfolio.js
+++ src/portfolio.js
@@ -226,16 +226,16 @@ const blogSection = {
   subtitle: "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
 
   blogs: [
-    // {
-    //   url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-    //   title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-    //   description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    // },
-    // {
-    //   url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-    //   title: "Why REACT is The Best?",
-    //   description: "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    // }
+    {
+      url: "https://opensource.creativecommons.org/blog/entries/cc-search-accessibility-and-internationalization/",
+      title: "CC Search, Proposal Drafting and Community Bonding",
+      description: "A good proposal: How I got selected for the Google Summer of Code in Creative Commons"
+    },
+    {
+      url: "https://opensource.creativecommons.org/blog/entries/cc-search-accessibility-week1-2/",
+      title: "CC Search, Setting up vue-i18n and internationalizing homepage",
+      description: "Setting up internationalization in the CC search website"
+    }
   ]
 };
 
